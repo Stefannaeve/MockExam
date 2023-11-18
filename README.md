8562b2c2ffd07a7d14c021d995c6fd60c8c3e747
Stefan <stefan.naeve@hotmail.com>
2023-11-18 14:45:52 +0100

    Made a readme, and added som logic to get Movies, and make a new one.

---
+++ README.md
@@ -0,0 +1,21 @@
+# Welcome to my web-development and api-design exam
+
+# MockExam Project
+
+## Introduction
+Welcome to the MockExam project! This application is designed to provide a comprehensive environment for conducting mock examinations. It's built using modern web technologies and offers a user-friendly interface for both exam administrators and participants.
+
+## Technologies Used
+- JavaScript
+- Node.js
+- React
+- Heroku
+- MongoDB
+
+## Installation
+To get the MockExam project up and running on your local machine, follow these steps:
+
+1. Install dependencies for both server and client:
+`npm install`
+2. Start the server and client concurrently:
+`npm run dev`
--- client/application.jsx
+++ client/application.jsx
@@ -2,6 +2,40 @@ import {BrowserRouter} from "react-router-dom";
 
 function homePage(){
 
+    return <div>
+        <h1>Home Page</h1>
+        <MovieList/>
+    </div>
+
+}
+
+function MovieList() {
+    const [movies, setMovies] = useState([]);
+    const [newMovie, setNewMovie] = useState('');
+
+    const addMovie = () => {
+        if (newMovie) {
+            setMovies([...movies, newMovie]);
+            setNewMovie('');
+        }
+    };
+
+    return (
+        <div>
+            <input
+                type="text"
+                value={newMovie}
+                onChange={(e) => setNewMovie(e.target.value)}
+                placeholder="Add a new movie"
+            />
+            <button onClick={addMovie}>Add Movie</button>
+            <ul>
+                {movies.map((movie, index) => (
+                    <li key={index}>{movie}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export function Application(){
